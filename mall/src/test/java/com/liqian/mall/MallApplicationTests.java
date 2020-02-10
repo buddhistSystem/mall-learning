a691a580ab8cf2f0029726f1b1a15a414f01166d
unknown <lq1083301982@163.com>
2020-02-10 10:01:30 +0800

    update

--- mall/src/test/java/com/liqian/mall/MallApplicationTests.java
+++ mall/src/test/java/com/liqian/mall/MallApplicationTests.java
@@ -1,14 +1,13 @@
 package com.liqian.mall;
 
 
-import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class MallApplicationTests {
+@RunWith(SpringRunner.class)
+public class MallApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
